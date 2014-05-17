572965f2c7be2f49e0f69e534a7ba88e384fb155
simoncorry <simoncorry@gmail.com>
2014-05-17 16:15:45 -0400

    Added sidebar menu

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html lang="en" class="ie9 ie" xmlns="http://www.w3.org/1999/xhtml"><![endif]-->
+<html lang="en" class="no-js">
+    <head>
+        <title>Science &amp; Creativity</title>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <!-- Viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <!-- Icons -->
+        <link rel="icon" type="image/x-icon" href="/assets/images/lib/icons/favicon.ico">
+        <link rel="shortcut icon" type="image/x-icon" href="/assets/images/lib/icons/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" href="/assets/images/lib/icons/apple-touch-icon-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/images/lib/icons/apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/images/lib/icons/apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/images/lib/icons/apple-touch-icon-144x144-precomposed.png">
+        <!-- CSS -->
+        <link type="text/css" rel="stylesheet" href="/assets/css/lib/reset.min.css">
+        <link type="text/css" rel="stylesheet" href="/assets/css/app/main.css">
+        <link type="text/css" rel="stylesheet" href="/assets/css/app/nav.css">
+        <!-- JS -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="/assets/js/lib/modernizr.js"></script>
+        <script type="text/javascript" src="/assets/js/lib/google-analytics.js"></script>
+        <script type="text/javascript" src="/assets/js/lib/sidebar-effects.js"></script>
+    </head>
+    <body>
+        <!-- Slideout Container -->
+        <div id="st-container">
+            <!-- Menu -->
+			<nav id="st-menu">
+				<h2>Menu</h2>
+				<ul>
+					<li><a href="#">Test</a></li>
+				</ul>
+			</nav>
+			<!-- Content Wrapper -->
+			<div id="st-pusher">
+				<div id="st-trigger-effects" class="st-content">
+				    <!-- Content -->
+				     
+				    <button data-effect="st-effect-12">Open door</button>
+				    
+				    <!-- /Content -->
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
