2ac97a675d29c3007f4b4631ca04a368ead65960
simoncorry <simoncorry@gmail.com>
2014-05-30 16:55:18 -0400

    Added header and footer

--- index.php
+++ index.php
@@ -17,12 +17,18 @@
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/images/lib/icons/apple-touch-icon-72x72-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/images/lib/icons/apple-touch-icon-114x114-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/images/lib/icons/apple-touch-icon-144x144-precomposed.png">
+        <!-- Google Fonts -->
+        <link type='text/css' rel='stylesheet' href='http://fonts.googleapis.com/css?family=Inconsolata:700,400'>
+        
         <!-- CSS -->
         <link type="text/css" rel="stylesheet" href="/assets/css/lib/reset.min.css">
         <link type="text/css" rel="stylesheet" href="/assets/css/lib/jquery.preloader-1.2.css">
         <link type="text/css" rel="stylesheet" href="/assets/css/app/main.css">
+        <link type="text/css" rel="stylesheet" href="/assets/css/app/typography.css">
+        <link type="text/css" rel="stylesheet" href="/assets/css/app/tooltips.css">
         <link type="text/css" rel="stylesheet" href="/assets/css/app/sidebar.css">
         <link type="text/css" rel="stylesheet" href="/assets/css/app/header.css">
+        <link type="text/css" rel="stylesheet" href="/assets/css/app/footer.css">
         <!-- JS -->
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script src="/assets/js/lib/modernizr.js"></script>
@@ -44,18 +50,59 @@
 			<nav id="sidebar_social" data-sidebar="sidebar_social" class="sidebar_pusher">
 				<ul>
 					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
+					<li><a href="#">Test</a></li>
 				</ul>
 			</nav>
 			<!-- Content Wrapper -->
 			<div id="content_pusher">
 			    <div class="content_shroud"></div>
 				<div id="wrapper_content">				    
-				    <!-- Content -->
-				    <section class="header">
-    				    <a class="sidebar_trigger" data-trigger="sidebar_main" style="pointer-events:all; display:block; width:80px; padding:20px; margin:0 auto;">Open door</a>
-    				    <a class="sidebar_trigger" data-trigger="sidebar_social" style="pointer-events:all; display:block; width:80px; padding:20px; margin:0 auto;">Open door</a>
+				    <!-- Header -->
+				    <section class="header clearfix">
+    				    <div class="cols lcol">
+        				    <a id="btn_social" class="btn_header sidebar_trigger" data-trigger="sidebar_social">
+        				        <div id="logo">
+            				        <? $a = array(9,8,6,8,9,8,9,8,9,8,6,8,9,8,6,8,9,8,9,8,9,8,6,8,9);
+                                        for( $i = 0; $i < count($a); $i++ ) : ?>
+                                        <div class="logo_pixel hover_times p<?=$a[$i]?>"></div>
+                                    <? endfor; ?>
+                                    <div class="moon"></div>
+                                    <div class="ship"></div>
+                                    <div class="exhaust"></div>
+                                    <div class="bg_image"></div>
+        				        </div>
+        				        <div class="btn_messages message_1">Science &amp; Creativity</div>
+        				    </a>       				    
+    				    </div>
+    				    <div class="cols rcol">
+    				        <a id="btn_main" class="btn_header sidebar_trigger tooltip tooltip_btn_main" data-trigger="sidebar_main" data-tooltip="Menu">
+    				            <div class="bars bar_top"></div>
+    				            <div class="bars bar_middle"></div>
+    				            <div class="bars bar_bottom"></div>
+    				        </a>
+    				    </div>
 				    </section>
-				    <!-- /Content -->							
+				    <!-- Header -->
+				    <section class="footer clearfix">
+    				    <div class="cols rcol">
+    				        <nav id="nav_network">
+    				            <ul>
+        				            <li><a href="http://twitter.com/simoncorry"  target="_blank" data-tooltip="Twitter"  class="tooltip tooltip_footer twitter">Read Tweets by Simon Corry</a></li>
+        				            <li><a href="http://github.com/simoncorry"   target="_blank" data-tooltip="Github"   class="tooltip tooltip_footer github">See code examples by Simon Corry</a></li>
+        				            <li><a href="http://dribbble.com/simoncorry" target="_blank" data-tooltip="Dribbble" class="tooltip tooltip_footer dribbble">View project shots by Simon Corry</a></li>
+    				            </ul>
+    				        </nav>
+    				    </div>
+				    </section>						
 				</div>
 			</div>
 		</div>
